f98b60c53da840e2036e3278d602f7d14f43f3b4
Miki_Levi <miclevi101@gmail.com>
2024-10-01 11:47:48 +0300

    server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,12 @@
-console.log("nodmon");
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello bro');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listen to PORT: http://localhost:${PORT}`);
+});
