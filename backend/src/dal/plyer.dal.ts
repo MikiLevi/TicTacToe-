9ada6888a91204a3ec0a17e754ca18f6c7a96545
oriori770 <oriori770@gmail.com>
2024-10-01 15:51:34 +0300

    nlnk

--- backend/src/dal/plyer.dal.ts
+++ backend/src/dal/plyer.dal.ts
@@ -1,14 +1,22 @@
 import {Plyer} from "../module/plyer.model";
 import {getFromDb, wirteToDb} from "./service.dal";
-const FILE_NAME = process.env.plyer_file_name as string;
+import {createPlyer} from"../service/plyer.service";
+import dotenv from "dotenv"
+dotenv.config();
+const FILE_NAME = process.env.PLYER_FILE_NAME as string;
+
+console.log(process.env.PORT);
 
 export async function getAllPlyers(): Promise<Plyer[]> {
     return getFromDb<Plyer>(FILE_NAME);
 }
 export async function addNewPlyerToDb(plyer:Plyer): Promise<void> {
-
-    const plyers:Plyer[] = await getFromDb<Plyer>("plyers.json");
+    console.log(FILE_NAME);
+    
+    const plyers:Plyer[] = await getFromDb<Plyer>(FILE_NAME);    
+    console.log("in", plyers);
     plyers.push(plyer);
     await wirteToDb<Plyer>(FILE_NAME, plyers);
 }
+
 
