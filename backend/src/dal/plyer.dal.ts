9632b43aa91d8df8b747dc8cbc2b8f46d9422b15
oriori770 <oriori770@gmail.com>
2024-10-01 15:17:40 +0300

    addNewPlyerToDb

--- backend/src/dal/plyer.dal.ts
+++ backend/src/dal/plyer.dal.ts
@@ -1,10 +1,14 @@
 import {Plyer} from "../module/plyer.model";
 import {getFromDb, wirteToDb} from "./service.dal";
+const FILE_NAME = process.env.plyer_file_name as string;
 
 export async function getAllPlyers(): Promise<Plyer[]> {
-    return getFromDb<Plyer>("plyers.json");
+    return getFromDb<Plyer>(FILE_NAME);
 }
-export async function addNewPlyer(): Promise<string> {
-    return getFromDb<Plyer>("plyers.json");
+export async function addNewPlyerToDb(plyer:Plyer): Promise<void> {
+
+    const plyers:Plyer[] = await getFromDb<Plyer>("plyers.json");
+    plyers.push(plyer);
+    await wirteToDb<Plyer>(FILE_NAME, plyers);
 }
 
