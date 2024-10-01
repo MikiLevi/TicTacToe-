414634989f9d5700f050016064b44dad86248c09
oriori770 <oriori770@gmail.com>
2024-10-01 16:06:45 +0300

    player name

--- backend/src/dal/plyer.dal.ts
+++ backend/src/dal/plyer.dal.ts
@@ -7,16 +7,16 @@ const FILE_NAME = process.env.PLYER_FILE_NAME as string;
 
 console.log(process.env.PORT);
 
-export async function getAllPlyers(): Promise<Plyer[]> {
-    return getFromDb<Plyer>(FILE_NAME);
+export async function getAllPlyers(): Promise<Player[]> {
+    return getFromDb<Player>(FILE_NAME);
 }
-export async function addNewPlyerToDb(plyer:Plyer): Promise<void> {
+export async function addNewPlayerToDb(Player:Player): Promise<void> {
     console.log(FILE_NAME);
     
-    const plyers:Plyer[] = await getFromDb<Plyer>(FILE_NAME);    
-    console.log("in", plyers);
-    plyers.push(plyer);
-    await wirteToDb<Plyer>(FILE_NAME, plyers);
+    const Players:Player[] = await getFromDb<Player>(FILE_NAME);    
+    console.log("in", Players);
+    Players.push(Player);
+    await wirteToDb<Player>(FILE_NAME, Players);
 }
 
 
